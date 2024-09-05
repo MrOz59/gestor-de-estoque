10d5fe11fc094b29f007bd5c02036809d51f6e9b
MrOz59 <oswaldo_de_souza@hotmail.com>
2024-09-05 10:13:57 -0300

    correção no main e no update mannager

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def main():
      # Configurar logs
     logger = configurar_logs()
     logger.info("Iniciando a aplicação...")
-    version = 'v1.5.4'
+    version = 'v1.5.5'
     os.environ['Version'] = version
     os.environ['SkipUpdate'] = 'False'
-    os.environ['Updater'] = 'v1.2.0'
+    os.environ['Updater'] = 'v1.1.1'
     os.environ['Owner'] = 'MrOz59'
     os.environ['Repo'] = 'kalymos'
     os.environ['MainExecutable'] = 'Kalymos.exe'
@@ -25,7 +25,7 @@ def main():
         logger.info("Argumento encontrado")
         # Ignora a verificação de atualização se --updated for encontrado
     else:
-        logger.info("Argumento encontrado")
+        logger.info("Argumento não encontrado")
         ensure_updater()
     
     try:
